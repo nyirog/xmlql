e4a7605120ab07dc40025830c47ed866c2a41531
Nyiro Gergo <gergo.nyiro@gmail.com>
2019-10-04 16:14:18 +0200

    book_store: write_xml

--- lib/xmlql/model/book_store.ex
+++ lib/xmlql/model/book_store.ex
@@ -8,14 +8,36 @@ defmodule Xmlql.Model.BookStore do
   Record.defrecord :book_type, Record.extract(:book_type, from: hrl_path)
   Record.defrecord :book_store, Record.extract(:book_store, from: hrl_path)
 
-  def parse_xml() do
+  def parse_xsd() do
     {:ok, xsd} = :erlsom.compile_xsd_file(Path.join([__DIR__, "BookStore.xsd"]))
+    xsd
+  end
+
+  def parse_xml(xsd) do
     {:ok, xml, _} = :erlsom.scan_file(Path.join([__DIR__, "BookStore.xml"]), xsd)
 
     store = book_store(xml)
     store[:book]
-    |> Enum.map(
-      fn book -> book_type(book) |> Enum.into(%{}) |> Map.delete(:anyAttribs) end
-    )
+    |> Enum.map(fn book -> book_type(book) |> Enum.into(%{}) end)
   end
+
+  def write_xml(books, xsd) do
+    book_type_keys = Keyword.keys(book_type(book_type))
+    book_types = Enum.map(
+      books,
+      fn book -> book_type_keys
+                 |> Enum.map(&Map.get(book, &1))
+                 |> List.insert_at(0, :book_type)
+                 |> List.to_tuple()
+      end
+    )
+
+    store = {:book_store, [], book_types}
+    {:ok, xml} = :erlsom.write(store, xsd, output: :binary)
+
+    {:ok, file} = File.open(Path.join([__DIR__, "BookStore.xml"]), [:write])
+    IO.binwrite(file, xml)
+    File.close(file)
+  end
+
 end
